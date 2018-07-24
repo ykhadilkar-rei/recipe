3d727ea8dd279894a7351704d8739c8d3f773aae
ykhadilkar-rei <ykhadilkar@reisys.com>
2018-07-24 16:22:25 -0400

    adding entities

---
+++ src/main/java/net/khadilkar/recipe/domain/UnitOfMeasure.java
@@ -0,0 +1,34 @@
+package net.khadilkar.recipe.domain;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+
+    public UnitOfMeasure() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
