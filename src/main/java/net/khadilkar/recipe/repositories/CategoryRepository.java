0756c28d89bb5ad17d9c6f7b0e8345258d527330
ykhadilkar-rei <ykhadilkar@reisys.com>
2018-07-26 09:59:58 -0400

    adding query methods

--- src/main/java/net/khadilkar/recipe/repositories/CategoryRepository.java
+++ src/main/java/net/khadilkar/recipe/repositories/CategoryRepository.java
@@ -3,8 +3,12 @@ package net.khadilkar.recipe.repositories;
 import net.khadilkar.recipe.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 /**
  * Created by Yatin on 7/25/18
  */
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
 }
