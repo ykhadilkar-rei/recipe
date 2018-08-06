1c5aff6d6f1b68d2799247a50727c56f79f40a9a
ykhadilkar-rei <ykhadilkar@reisys.com>
2018-08-06 13:29:23 -0400

    adding unit tests

---
+++ src/test/java/net/khadilkar/recipe/controllers/IndexControllerTest.java
@@ -0,0 +1,39 @@
+package net.khadilkar.recipe.controllers;
+
+import net.khadilkar.recipe.services.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+public class IndexControllerTest {
+    IndexController indexController;
+
+    @Mock
+    RecipeService recipeService;
+
+    @Mock
+    Model model;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    public void getIndexPage() {
+        String indexPage = indexController.getIndexPage(model);
+
+        assertEquals("index",indexPage);
+        verify(recipeService,times(1)).getRecipes();
+        verify(model,times(1)).addAttribute(eq("recipes"),anySet());
+    }
+}
---
+++ src/test/java/net/khadilkar/recipe/domain/CategoryTest.java
@@ -0,0 +1,34 @@
+package net.khadilkar.recipe.domain;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CategoryTest {
+
+    Category category;
+
+    @Before
+    public void setUp(){
+        category = new Category();
+    }
+
+    @Test
+    public void getId() {
+        Long idValue = 4L;
+
+        category.setId(idValue);
+
+        assertEquals(idValue, category.getId());
+
+    }
+
+    @Test
+    public void getDescription() {
+    }
+
+    @Test
+    public void getRecipes() {
+    }
+}
